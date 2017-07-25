a367a12ca0d4b8fc517c1c9074f0332fc0797a37
curtis ballard <curtisballard@curtiss-MacBook-Air.local>
2017-07-24 23:24:23 -0700

    friend-finder-cuhz

---
+++ friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'coolin cuhz carl',
+		photo: 'https://img.ifcdn.com/images/bddd6fd522d6a3e473fe220a9e324816752cbf97d04bae402dc277f1698c2482_1.jpg',
+		scores: [5, 1, 4, 4, 5, 1]
+	},{
+		name: 'keke',
+		photo: 'https://s-media-cache-ak0.pinimg.com/736x/26/19/72/2619724294ae0635a5c27113a4abd888--weave-hairstyles-crazy-hairstyles.jpg',
+		scores: [4, 2, 4, 5, 3, 2]
+	},{
+		name: 'whats brackin capin',
+		photo: 'https://ecarson.files.wordpress.com/2014/12/bloods.jpg',
+		scores: [3, 3, 3, 3, 3, 3]
+	},{
+		name: 'tyesha',
+		photo: 'http://weavesgalore.weebly.com/uploads/1/9/4/8/19487871/8497152_orig.jpg',
+		scores: [5, 1, 3, 2, 2, 4]
+	}];
+
+module.exports = friends;
